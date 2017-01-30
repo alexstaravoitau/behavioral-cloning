77ee993df9cceadc62f2b3fcbe8b791e286a6cfa
Alex Staravoitau <alex.staravoitau@gmail.com>
2017-01-30 11:30:49 +0000

    Improve shadow augmentation.
    Improve shadow augmentation by shadowing a vertical slice of the image rather than adding a box.

--- data.py
+++ data.py
@@ -17,7 +17,7 @@ def preprocess(image, top_offset=0.375, bottom_offset=0.125):
     return image
 
 def generate_samples(data, root_path, augment=True):
-    while 1:
+    while True:
         # Generate random batch of indices
         indices = np.random.permutation(data.count()[0])
         batch_size = 128
@@ -29,20 +29,34 @@ def generate_samples(data, root_path, augment=True):
             # Read in and preprocess a batch of images
             for i in batch_indices:
                 # Randomly select camera
-                #camera = np.random.choice(len(cameras), p=[.25, .5, .25])
                 camera = np.random.randint(len(cameras)) if augment else 1
                 # Read frame image and work out steering angle
                 image = mpimg.imread(os.path.join(root_path, data[cameras[camera]].values[i].strip()))
                 angle = data.steering.values[i] + cameras_steering_correction[camera]
                 if augment:
                     # Add random shadow
-                    x1, y1 = random.randint(0, image.shape[1]), random.randint(0, image.shape[0])
-                    x2, y2 = random.randint(x1, image.shape[1]), random.randint(y1, image.shape[0])
-                    image[y1:y2, x1:x2, :] = (image[y1:y2, x1:x2, :] * .5).astype(np.int32)
-                    # Randomly shift up and down while preprocessing
-                top = random.uniform(.375 - .075, .375 + .075) if augment else .375
-                bottom = random.uniform(.125 - .075, .125 + 0.075) if augment else .125
-                image = preprocess(image, top_offset=top, bottom_offset=bottom)
+                    h, w = image.shape[0], image.shape[1]
+                    [x1, x2] = np.random.choice(w, 2, replace=False)
+                    k = h / (x2 - x1)
+                    b = - k * x1
+                    shadow_adjustment = random.uniform(.25, .75)
+                    non_shadow_adjustment = random.uniform(.5, 1.5)
+                    for i in range(h):
+                        c = int((i - b) / k)
+                        # Randomly decrease brightness for shadowed part
+                        image[i, :c, :] = (image[i, :c, :] * shadow_adjustment).astype(np.int32)
+                        # And apply random brightness adjustment to the rest of the image
+                        image[i, c:, :] = (np.minimum(
+                            np.ones_like(image[i, c:, :]) * 255,
+                            image[i, c:, :] * non_shadow_adjustment
+                        )).astype(np.int32)
+                # Randomly shift up and down while preprocessing
+                v_delta = .075 if augment else 0
+                image = preprocess(
+                    image,
+                    top_offset=random.uniform(.375 - v_delta, .375 + v_delta),
+                    bottom_offset=random.uniform(.125 - v_delta, .125 + v_delta)
+                )
                 # Append to batch
                 x = np.append(x, [image], axis=0)
                 y = np.append(y, [angle])
