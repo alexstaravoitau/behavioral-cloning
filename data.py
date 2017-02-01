f5e9775e8c775a4470227390d4abe472c24d6781
Alex Staravoitau <alex.staravoitau@gmail.com>
2017-02-01 22:31:37 +0000

    Update preprocessing.
    Update preprocessing to match simplified model.

--- data.py
+++ data.py
@@ -8,12 +8,12 @@ import os
 
 # Cameras we will use
 cameras = ['left', 'center', 'right']
-cameras_steering_correction = [.2, 0., -.2]
+cameras_steering_correction = [.25, 0., -.25]
 
 def preprocess(image, top_offset=.375, bottom_offset=.125):
     top = int(top_offset * image.shape[0])
     bottom = int(bottom_offset * image.shape[0])
-    image = sktransform.resize(image[top:-bottom, :], (66, 200, 3))
+    image = sktransform.resize(image[top:-bottom, :], (32, 128, 3))
     return image
 
 def generate_samples(data, root_path, augment=True):
@@ -24,7 +24,7 @@ def generate_samples(data, root_path, augment=True):
         for batch in range(0, len(indices), batch_size):
             batch_indices = indices[batch:(batch + batch_size)]
             # Output arrays
-            x = np.empty([0, 66, 200, 3], dtype=np.float32)
+            x = np.empty([0, 32, 128, 3], dtype=np.float32)
             y = np.empty([0], dtype=np.float32)
             # Read in and preprocess a batch of images
             for i in batch_indices:
@@ -39,19 +39,12 @@ def generate_samples(data, root_path, augment=True):
                     [x1, x2] = np.random.choice(w, 2, replace=False)
                     k = h / (x2 - x1)
                     b = - k * x1
-                    shadow_adjustment = random.uniform(.4, .6)
-                    non_shadow_adjustment = random.uniform(.75, 1.25)
                     for i in range(h):
                         c = int((i - b) / k)
                         # Randomly decrease brightness for shadowed part
-                        image[i, :c, :] = (image[i, :c, :] * shadow_adjustment).astype(np.int32)
-                        # And apply random brightness adjustment to the rest of the image
-                        image[i, c:, :] = (np.minimum(
-                            np.ones_like(image[i, c:, :]) * 255,
-                            image[i, c:, :] * non_shadow_adjustment
-                        )).astype(np.int32)
+                        image[i, :c, :] = (image[i, :c, :] * .5).astype(np.int32)
                 # Randomly shift up and down while preprocessing
-                v_delta = .1 if augment else 0
+                v_delta = .05 if augment else 0
                 image = preprocess(
                     image,
                     top_offset=random.uniform(.375 - v_delta, .375 + v_delta),
