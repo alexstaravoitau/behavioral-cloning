fff6e6a7e0b8c3d0de7cad0786916b7e318aea31
Alex Staravoitau <alex.staravoitau@gmail.com>
2017-02-01 22:31:03 +0000

    Simplify model.
    Simplify model. A lot!

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 import pandas as pd
 tf.python.control_flow_ops = tf
 from keras import models, optimizers, backend
-from keras.layers import core, convolutional
+from keras.layers import core, convolutional, pooling
 from sklearn import model_selection
 from data import generate_samples, preprocess
 from weights_logger_callback import WeightsLogger
@@ -20,26 +20,25 @@ if __name__ == '__main__':
     df_train, df_valid = model_selection.train_test_split(df, test_size=.2)
 
     model = models.Sequential()
-    model.add(convolutional.Convolution2D(24, 5, 5, subsample=(2, 2), input_shape=(66, 200, 3), activation='relu'))
-    model.add(convolutional.Convolution2D(36, 5, 5, subsample=(2, 2), activation='relu'))
-    model.add(convolutional.Convolution2D(48, 3, 3, activation='relu'))
+    model.add(convolutional.Convolution2D(16, 3, 3, input_shape=(32, 128, 3), activation='relu'))
+    model.add(pooling.MaxPooling2D(pool_size=(2, 2)))
+    model.add(convolutional.Convolution2D(32, 3, 3, activation='relu'))
+    model.add(pooling.MaxPooling2D(pool_size=(2, 2)))
     model.add(convolutional.Convolution2D(64, 3, 3, activation='relu'))
-    model.add(convolutional.Convolution2D(64, 3, 3, activation='relu'))
+    model.add(pooling.MaxPooling2D(pool_size=(2, 2)))
     model.add(core.Flatten())
-    model.add(core.Dense(1164, activation='relu'))
+    model.add(core.Dense(500, activation='relu'))
     model.add(core.Dropout(.5))
     model.add(core.Dense(100, activation='relu'))
-    model.add(core.Dropout(.5))
-    model.add(core.Dense(50, activation='relu'))
-    model.add(core.Dropout(.5))
-    model.add(core.Dense(10, activation='relu'))
+    model.add(core.Dropout(.25))
+    model.add(core.Dense(20, activation='relu'))
     model.add(core.Dense(1))
     model.compile(optimizer=optimizers.Adam(lr=1e-04), loss='mean_squared_error')
 
     history = model.fit_generator(
         generate_samples(df_train, local_data_path),
         samples_per_epoch=df_train.shape[0],
-        nb_epoch=20,
+        nb_epoch=30,
         validation_data=generate_samples(df_valid, local_data_path, augment=False),
         callbacks=[WeightsLogger(root_path=local_project_path)]
         nb_val_samples=df_valid.shape[0],
